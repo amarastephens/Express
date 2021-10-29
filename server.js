c817a5df322fee464fe816b9a45a9c788627b2da
Amara Stephens <amarastephens@Amaras-MacBook-Pro.local>
2021-10-28 22:28:04 -0500

    fin

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const { dirname } = require("path")
+let app = express()
+const path = require("path")
+
+app.get('/', (req, res, next) => {
+    res.send("Hello from the web server side!")
+})
+
+app.listen(3000)
+
+app.use(express.static(path.join(__dirname,"../public")))
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, "../public/index.html" ))
+// })
+
+// app.get('/css/styles.css', (req, res) => {
+//     res.sendFile(path.join(__dirname, "../public/css/styles.css"))
+// })
+
+// app.get('/css/styles.css', (req, res) => {
+//     res.sendFile(path.join(__dirname, "../public/css/styles.css"))
+// })
+
+app.use((req, res, next) => {
+console.log(req.url)
+    next()
+})
